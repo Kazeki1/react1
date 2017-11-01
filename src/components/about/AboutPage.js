136d7bc55cc58b5cffec141171317de2f1edf1e5
kazeki1 <wmj.floyd@gmail.com>
2017-11-01 06:30:20 -0600

    react routing with header

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,17 @@
+/**
+ * Created by kazeki on 17/10/31.
+ */
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This application uses React, REDux, React Router and a variety of other helpful libraries</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
